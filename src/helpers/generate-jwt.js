7723786994b2ec2258d15bdceeb0f07a8060fd84
Mateo Morelo <mmoreloidep@gmail.com>
2026-01-11 23:35:11 -0500

    Se cambia el user.controller, se ajusta login, se agrega jwt, se crea el crea personas, me genera la persona y usuario, se agregan validaciones a la ruta

---
+++ src/helpers/generate-jwt.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken'
+
+
+export const generateJwt = (peoId  = '', usu_identification = '', usu_name1 = '', usu_lastname1 = '', usu_correo = '', usu_role = '', usu_state = ''  ) => {
+
+    return new Promise((resolve, reject) => {
+
+        const payload = { peoId, usu_identification, usu_name1, usu_lastname1, usu_correo, usu_role, usu_state }
+        jwt.sign(payload, process.env.JWT_SECRET, {  expiresIn: '8h'}, (error, token) => {
+            
+            if (error) {
+                console.log(error)
+
+                reject('Error a la hora de generar el token')
+            } else {
+
+                resolve(token)
+            }
+
+
+        })
+    })
+
+}   
---
+++ src/helpers/index.js
@@ -0,0 +1,18 @@
+
+// import {ExistIdProduct} from './exist-idProducts.js'
+
+// import {existIdentificacion} from './existCedula.js'
+
+// import { existEmail } from './existEmail.js'
+
+import {generateJwt} from './generate-jwt.js'
+
+
+export default {
+
+    // ExistIdProduct,
+    // existIdentificacion,
+    // existEmail,
+    generateJwt
+  
+}
