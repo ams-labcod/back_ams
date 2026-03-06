930129de0ce368cbfebcc49513fd38fc7473c871
Juan Afanador <juan.afanador@actiobyte.com>
2026-03-06 15:27:38 -0500

    cambios db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,6 @@
-// import { createPool } from "mysql2/promise";
-// import '../utils/env.js'
+import { createPool } from "mysql2/promise";
+import '../utils/env.js'
+
 
 //  export const pool = createPool({
 //      host: process.env.HOST_DEV,
@@ -12,5 +13,6 @@ export const pool = createPool(
 //  'mysql://ue6lzi4rzrwbgeew:p3DNp4j6ogym2GBpeY9n@blj394itpypibv4ybum6-mysql.services.clever-cloud.com:3306/blj394itpypibv4ybum6' -> CLEVER
  'mysql://root:ezhOhvAzRKHMePpLeQrTysKoutsQjnqQ@interchange.proxy.rlwy.net:29405/railway' //-> RAILWAY
 );
+
 
 export default pool;
