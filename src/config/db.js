9d1d750932c76afc8ea3209efcf74888392f3b94
MateoML10 <102306340+MateoML10@users.noreply.github.com>
2026-03-05 21:14:47 -0500

    Update database connection configuration

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,15 @@
 import { createPool } from "mysql2/promise";
 import '../utils/env.js'
 
- export const pool = createPool({
-     host: process.env.HOST_DEV,
-     user: process.env.USER_DEV ,
-     password: process.env.PASSWORD_DEV,
-     database: process.env.DATABASE_DEV
- })
+// export const pool = createPool({
+//     host: process.env.HOST_DEV,
+//     user: process.env.USER_DEV ,
+//     password: process.env.PASSWORD_DEV,
+//     database: process.env.DATABASE_DEV
+// })
 
-//export const pool = createPool(
- // 'mysql://ue6lzi4rzrwbgeew:p3DNp4j6ogym2GBpeY9n@blj394itpypibv4ybum6-mysql.services.clever-cloud.com:3306/blj394itpypibv4ybum6'
-//);
+export const pool = createPool(
+  'mysql://ue6lzi4rzrwbgeew:p3DNp4j6ogym2GBpeY9n@blj394itpypibv4ybum6-mysql.services.clever-cloud.com:3306/blj394itpypibv4ybum6'
+);
 
 export default pool;
