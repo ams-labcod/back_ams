e8715d43bb40096d44a68bbdbb76fa5bb94451b1
Juan Afanador <juan.afanador@actiobyte.com>
2026-03-12 15:58:45 -0500

    getAll evaluationType

--- src/routes/evaluationType.route.js
+++ src/routes/evaluationType.route.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 import { verifyJwt } from '../middlewares/verifyToken.js';
 
-import { createEvaluationType } from "../controller/evaluationType.controller.js";
+import { createEvaluationType, getAllEvaluationType } from "../controller/evaluationType.controller.js";
 
 import { validateRoles } from "../middlewares/validateRols.js";
 
@@ -10,5 +10,7 @@ const router = Router()
 
 //* CREERIA QUE EL TIPO DE EVALUACION LA CREA UN DOCENTE
 router.post('/createEvaluationType', verifyJwt, validateRoles('ROL_ADMIN','ROL_TEACHER'), createEvaluationType) 
+
+router.get('/getAllEvaluationType', verifyJwt, validateRoles('ROL_ADMIN', 'ROL_ADMINISTRATIVO', 'ROL_TEACHER', 'ROL_STUDENT'), getAllEvaluationType)
 
 export default router
