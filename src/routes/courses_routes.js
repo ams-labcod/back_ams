cb2bb37e71635978781b94470dc2218e55b1ef64
Juan Afanador <juan.afanador@actiobyte.com>
2026-03-17 15:17:28 -0500

    remove validations -> CreateCourse

--- src/routes/courses_routes.js
+++ src/routes/courses_routes.js
@@ -40,121 +40,122 @@ const CURSOS_POR_NIVEL = {
   BASICA: [1, 2, 3, 4, 5],
   SECUNDARIA: [6, 7, 8, 9, 10, 11]
 };
+
 router.post(
   '/create_course',
   verifyJwt,validateRoles('ROL_ADMIN','ROL_ADMINISTRATIVO'),
-  [
+  // [
 
-    /* ===============================
-       NIVEL
-    =============================== */
-    check('cou_level')
-      .notEmpty().withMessage('El nivel es obligatorio')
-      .isIn(['PREESCOLAR', 'BASICA', 'SECUNDARIA'])
-      .withMessage('Nivel inválido. Valores permitidos: PREESCOLAR, BASICA, SECUNDARIA'),
+  //   /* ===============================
+  //      NIVEL
+  //   =============================== */
+  //   check('cou_level')
+  //     .notEmpty().withMessage('El nivel es obligatorio')
+  //     .isIn(['PREESCOLAR', 'BASICA', 'SECUNDARIA'])
+  //     .withMessage('Nivel inválido. Valores permitidos: PREESCOLAR, BASICA, SECUNDARIA'),
 
-    /* ===============================
-     NOMBRE DEL CURSO
-  =============================== */
-    check('cou_name_teach')
-      .notEmpty().withMessage('El nombre del curso es obligatorio')
-      .isString()
-      .custom((value, { req }) => {
-        const level = req.body.cou_level;
-        const num = req.body.cou_num_courses;
-        const name = value.toUpperCase();
+  //   /* ===============================
+  //    NOMBRE DEL CURSO
+  // =============================== */
+  //   check('cou_name_teach')
+  //     .notEmpty().withMessage('El nombre del curso es obligatorio')
+  //     .isString()
+  //     .custom((value, { req }) => {
+  //       const level = req.body.cou_level;
+  //       const num = req.body.cou_num_courses;
+  //       const name = value.toUpperCase();
 
-        // PREESCOLAR
-        if (level === 'PREESCOLAR') {
-          const permitidos = GRADOS.PREESCOLAR.join(', ');
+  //       PREESCOLAR
+  //       if (level === 'PREESCOLAR') {
+  //         const permitidos = GRADOS.PREESCOLAR.join(', ');
 
-          if (!GRADOS.PREESCOLAR.includes(name)) {
-            throw new Error(
-              `Curso inválido para PREESCOLAR. Valores permitidos: ${permitidos}`
-            );
-          }
-          return true;
-        }
+  //         if (!GRADOS.PREESCOLAR.includes(name)) {
+  //           throw new Error(
+  //             `Curso inválido para PREESCOLAR. Valores permitidos: ${permitidos}`
+  //           );
+  //         }
+  //         return true;
+  //       }
 
-        // BÁSICA
-        if (level === 'BASICA') {
-          const permitidos = Object.values(GRADOS.BASICA).join(', ');
+  //       BÁSICA
+  //       if (level === 'BASICA') {
+  //         const permitidos = Object.values(GRADOS.BASICA).join(', ');
 
-          if (GRADOS.BASICA[num] !== name) {
-            throw new Error(
-              `Curso inválido para BÁSICA. Valores permitidos: ${permitidos}`
-            );
-          }
-          return true;
-        }
+  //         if (GRADOS.BASICA[num] !== name) {
+  //           throw new Error(
+  //             `Curso inválido para BÁSICA. Valores permitidos: ${permitidos}`
+  //           );
+  //         }
+  //         return true;
+  //       }
 
-        // SECUNDARIA
-        if (level === 'SECUNDARIA') {
-          const permitidos = Object.values(GRADOS.SECUNDARIA).join(', ');
+  //       SECUNDARIA
+  //       if (level === 'SECUNDARIA') {
+  //         const permitidos = Object.values(GRADOS.SECUNDARIA).join(', ');
 
-          if (GRADOS.SECUNDARIA[num] !== name) {
-            throw new Error(
-              `Curso inválido para SECUNDARIA. Valores permitidos: ${permitidos}`
-            );
-          }
-          return true;
-        }
+  //         if (GRADOS.SECUNDARIA[num] !== name) {
+  //           throw new Error(
+  //             `Curso inválido para SECUNDARIA. Valores permitidos: ${permitidos}`
+  //           );
+  //         }
+  //         return true;
+  //       }
 
-        return true;
-      }),
+  //       return true;
+  //     }),
 
 
 
-    check('cou_num_courses')
-      .custom((value, { req }) => {
-        const level = req.body.cou_level;
-        const num = Number(value);
+  //   check('cou_num_courses')
+  //     .custom((value, { req }) => {
+  //       const level = req.body.cou_level;
+  //       const num = Number(value);
 
-        // PREESCOLAR
-        if (level === 'PREESCOLAR') {
-          if (value !== undefined && value !== null && value !== '') {
-            throw new Error(
-              'PREESCOLAR no maneja número de curso. Valores permitidos: (vacío)'
-            );
-          }
-          return true;
-        }
+  //       PREESCOLAR
+  //       if (level === 'PREESCOLAR') {
+  //         if (value !== undefined && value !== null && value !== '') {
+  //           throw new Error(
+  //             'PREESCOLAR no maneja número de curso. Valores permitidos: (vacío)'
+  //           );
+  //         }
+  //         return true;
+  //       }
 
-        // Obligatorio para otros niveles
-        if (value === undefined || value === null || value === '') {
-          throw new Error(
-            `El número del curso es obligatorio. Valores permitidos para ${level}: ${CURSOS_POR_NIVEL[level]?.join(', ')
-            }`
-          );
-        }
+  //       Obligatorio para otros niveles
+  //       if (value === undefined || value === null || value === '') {
+  //         throw new Error(
+  //           `El número del curso es obligatorio. Valores permitidos para ${level}: ${CURSOS_POR_NIVEL[level]?.join(', ')
+  //           }`
+  //         );
+  //       }
 
-        if (isNaN(num)) {
-          throw new Error(
-            `El número del curso debe ser numérico. Valores permitidos para ${level}: ${CURSOS_POR_NIVEL[level]?.join(', ')
-            }`
-          );
-        }
+  //       if (isNaN(num)) {
+  //         throw new Error(
+  //           `El número del curso debe ser numérico. Valores permitidos para ${level}: ${CURSOS_POR_NIVEL[level]?.join(', ')
+  //           }`
+  //         );
+  //       }
 
-        // Validación por nivel
-        if (!CURSOS_POR_NIVEL[level]?.includes(num)) {
-          throw new Error(
-            `Número de curso inválido para ${level}. Valores permitidos: ${CURSOS_POR_NIVEL[level].join(', ')
-            }`
-          );
-        }
+  //       Validación por nivel
+  //       if (!CURSOS_POR_NIVEL[level]?.includes(num)) {
+  //         throw new Error(
+  //           `Número de curso inválido para ${level}. Valores permitidos: ${CURSOS_POR_NIVEL[level].join(', ')
+  //           }`
+  //         );
+  //       }
 
-        return true;
-      }),
+  //       return true;
+  //     }),
 
-    /* ===============================
-       MATERIAS / TEMAS
-    =============================== */
-    // check('cou_theme')
-    //   .notEmpty().withMessage('Las materias del curso son obligatorias')
-    //   .isString()
+  //   /* ===============================
+  //      MATERIAS / TEMAS
+  //   =============================== */
+  //   check('cou_theme')
+  //     .notEmpty().withMessage('Las materias del curso son obligatorias')
+  //     .isString()
 
-  ],
-  validations,
+  // ],
+  // validations,
   createCourse
 
 );
