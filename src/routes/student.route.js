4d81bc557d55be8b04e513313730a20ca62daebb
Juan Afanador <juan.afanador@actiobyte.com>
2026-03-18 14:53:35 -0500

    get all student

--- src/routes/student.route.js
+++ src/routes/student.route.js
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { verifyJwt } from '../middlewares/verifyToken.js';
 
 import { validateRoles } from "../middlewares/validateRols.js";
+
 import { getAllStudents, getStudentSubjects } from "../controller/student.controller.js";
 
 
@@ -15,6 +16,6 @@ router.get(
   getStudentSubjects
 );
 
-router.get('/getAllStudents', verifyJwt, validateRoles('ROL_TEACHER', 'ROL_ADMIN', 'ROL_ADMINISTRATIVO'), getAllStudents)
+router.get('/student/getAll', verifyJwt, validateRoles('ROL_TEACHER', 'ROL_ADMIN', 'ROL_ADMINISTRATIVO'), getAllStudents)
 
 export default router
